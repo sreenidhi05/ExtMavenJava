ff8ce51e94df312623c698e5a91ce5b30865698b
sreenidhi <sarojasreenidhi@gmail.com>
2025-01-15 12:41:29 +0530

    Ext practice Maven Java

---
+++ src/main/java/com/app/demo/Ext_MavenJava/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.Ext_MavenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "SE Practice for External" );
+    }
+}
---
+++ src/test/java/com/app/demo/Ext_MavenJava/AppTest.java
@@ -0,0 +1,20 @@
+package com.app.demo.Ext_MavenJava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
